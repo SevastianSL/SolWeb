da2e1fc28cc0a1e65db0064febb2925c8e19c052
SevastianSL <sevastiansalcedo@hotmail.com>
2021-06-03 15:06:43 -0500

    5

--- T2Proyecto/src/main/java/com/amontanez/demo/dao/ITiendaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.amontanez.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.amontanez.demo.entity.Tienda;
-
-public interface ITiendaDAO extends CrudRepository<Tienda, Long> {
-	
-}
